57b21fce3564fc9dc396e8332c93dcbc992a9d67
KevinW5 <kevinw267@nycstudents.net>
2024-06-06 21:27:13 -0400

    Update GridPath.java

--- frq Solutions/q4/GridPath.java
+++ frq Solutions/q4/GridPath.java
@@ -7,55 +7,30 @@ public class GridPath {
     this.grid = grid;
   }
 
-  // PART A (uncomment first)
+  // PART A
   public Location getNextLoc(int row, int col) {
-    boolean hasRight = false;
-    boolean hasBelow = false;
-    int rightValue = Integer.MAX_VALUE;
-    int belowValue = Integer.MAX_VALUE;
-    int rightRow = row;
-    int rightCol = col + 1;
-    int belowRow = row + 1;
-    int belowCol = col;
-    if (col + 1 < grid[0].length) {
-      hasRight = true;
-      rightValue = grid[row][col + 1];
+    if (col + 1 < grid[0].length && (row + 1 >= grid.length || grid[row][col + 1] < grid[row + 1][col])) {
+      return new Location(row, col + 1);
+    } else if (row + 1 < grid.length) {
+      return new Location(row + 1, col);
     }
-    if (row + 1 < grid.length) {
-      hasBelow = true;
-      belowValue = grid[row + 1][col];
-    }
-        if (hasRight && hasBelow) {
-      if (rightValue < belowValue) {
-        return new Location(rightRow, rightCol);
-      } else {
-        return new Location(belowRow, belowCol);
-      }
-    } else if (hasRight) {
-      return new Location(rightRow, rightCol);
-    } else {
-      return new Location(belowRow, belowCol);
-    }
-   }
+    return null;
+  }
 
-
-  
   // PART B
   public int sumPath(int row, int col) {
-   int sum = grid[row][col];
-
-    while (row < grid.length - 1 && col < grid[0].length - 1) {
+    int sum = grid[row][col];
+    while (row < grid.length - 1 || col < grid[0].length - 1) {
       Location nextLoc = getNextLoc(row, col);
+      if (nextLoc == null) break;
       row = nextLoc.getRow();
       col = nextLoc.getCol();
       sum += grid[row][col];
     }
-
     return sum;
-}
+  }
 
   public String toString() {
     return Arrays.deepToString(grid);
   }
-  
 }
