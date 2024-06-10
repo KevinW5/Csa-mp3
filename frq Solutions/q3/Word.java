0f5599b3d322a127dc0b71997e9f14acefddd5b4
KevinW5 <kevinw267@nycstudents.net>
2024-06-09 21:28:39 -0400

    Update Word.java

--- frq Solutions/q3/Word.java
+++ frq Solutions/q3/Word.java
@@ -5,22 +5,25 @@ public class Word {
   public Word(ArrayList<String> words) {
     wordList = words;
   }
-
+// Part A
   public boolean isWordChain() {
     for (int i = 1; i < wordList.size(); i++) {
+      // Check if every element in wordList except the first contains the previous substring element
       if(wordList.get(i).indexOf(0)==-1){
         return false;
       }
     }
     return true;
   }
-
+//Part B
   public ArrayList<String> createList(String target) {
+        // New list
         ArrayList<String> result = new ArrayList<String>();
         int tarlen = target.length();
-
         for (String word : wordList) {
+          // check if each word of the list have length greater than the target and if it contain the target in the front
             if (word.length() >= tarlen && word.substring(0, tarlen).equals(target)) {
+              // removes the target in front if true
                 result.add(word.substring(tarlen));
             }
         }
